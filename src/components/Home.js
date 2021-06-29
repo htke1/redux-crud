d464380609050cae79d102b9bb802e660ef888cb
htke1 <sheraptenzin98@gmail.com>
2021-06-29 12:43:04 +0530

    Removed extra search component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Heading } from "./Heading";
 import { UserList } from "./UserList";
-import SearchBox from "./Search"
 export const Home = () => {
   return (
     <>
       <Heading />
-      {/* <SearchBox /> */}
       <UserList />
     </>
   )
